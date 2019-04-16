3dcb8aa85052595659ee26aa4cb1a511f25decc5
Jeremias Roessler <roessler@retest.de>
2019-04-16 22:10:00 +0200

    First implementation to output something reasonable
    Suite: RecheckWebDemo
    Test: First
    Command: driver.get( "https://www.wikipedia.org" );
    Command: driver.findElement( By.cssSelector(".mw-search-form-wrapper" ) ).click();
    Command: setWindowSize
    Command: driver.findElement( By.id("searchInput" ) ).click();
    Command: driver.findElement( By.id("searchInput" ) ).sendKeys( "Golden Master Testing" );
    Command: driver.findElement( By.cssSelector(".pure-button" ) ).click();

--- app/convert.ts
+++ app/convert.ts
@@ -1,4 +1,47 @@
 
 import * as side from '/Users/roessler/Documents/startup/workspace/side2java/app/example.json';
-const word = (<any>side).name;
-console.log(word); // output 'testing'
+
+console.log("Suite: " + camelize(side.name));
+for (var testCount in side.tests) {
+  console.log("Test: " + camelize(side.tests[testCount].name));
+  for (var commandCount in side.tests[testCount].commands) {
+    console.log("Command: " + toCommandString(side, testCount, commandCount));
+  }
+}
+
+function camelize(name: string) {
+  return name.replace(/(?:^\w|[A-Z]|\b\w)/g, function(word: String) {
+    return word.toUpperCase();
+  }).replace(/[^a-z ]/ig, '');
+}
+
+interface By {
+    selector: string;
+    identifier: string;
+}
+
+function toCommandString(side: any, testCount: string, commandCount: string) {
+  switch (side.tests[testCount].commands[commandCount].command) {
+  case "open":
+    return "driver.get( \"" + side.url + "\" );";
+  case "click": {
+      let by: By = getBy(side.tests[testCount].commands[commandCount].target);
+      return "driver.findElement( By." + by.selector + "(\"" + by.identifier + "\" ) ).click();";
+    }
+  case "type": {
+      let by: By = getBy(side.tests[testCount].commands[commandCount].target);
+      return "driver.findElement( By." + by.selector + "(\"" + by.identifier + "\" ) ).sendKeys( \"" + side.tests[testCount].commands[commandCount].value + "\" );";
+    }
+  }
+  return side.tests[testCount].commands[commandCount].command;
+}
+
+function getBy(target: string){
+  if (target.startsWith("css=")) {
+    return {selector: "cssSelector", identifier: target.replace("css=", "")};
+  }
+  if (target.startsWith("id=")) {
+    return {selector: "id", identifier: target.replace("id=", "")};
+  }
+  throw new Error("getBy not implemented for " + target);
+}
