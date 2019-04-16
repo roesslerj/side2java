c89671f9ba17bbdef467b3cce1b6aa30602c2e2f
Jeremias Roessler <roessler@retest.de>
2019-04-16 21:02:18 +0200

    Code to read the example recording in json (rename file)

--- app/convert.ts
+++ app/convert.ts
@@ -1,1 +1,4 @@
-import side = require('./example.side');
+
+import * as side from '/Users/roessler/Documents/startup/workspace/side2java/app/example.json';
+const word = (<any>side).name;
+console.log(word); // output 'testing'
